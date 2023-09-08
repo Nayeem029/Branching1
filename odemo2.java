9df101f71e1780b7f8f1c9a346bd57857737e157
nayeem <nayeemshaik568@gmail.com>
2023-09-08 07:26:25 +0530

    Hi this is Arif

---
+++ odemo2.java
@@ -0,0 +1,19 @@
+package nayeem;
+
+public class odemo2 
+{
+	int x,y;
+	void fun1()
+	{
+		x=x+1;
+		odemo2 d1 = new odemo2();
+		d1.x =x+2;
+	}
+
+	public static void main(String[] args) 
+	{
+		odemo2 d1= new odemo2();
+		d1.fun1();
+		System.out.println(d1.x);
+	}
+}
